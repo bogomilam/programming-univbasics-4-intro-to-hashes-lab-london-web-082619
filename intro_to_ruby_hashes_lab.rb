130452f1e4176f469a52bb4fbdddf357a66d00dc
bogomilmihaylov <bogomilmihaylov@live.com>
2019-08-17 19:14:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,6 +20,11 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
+ my_hash_creator = {
+   :key => "k1"
+   
+ }
+ 
   # return a hash that includes the key and value parameters passed into this method
 end
 
